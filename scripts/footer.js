87f8c282af17f2fbb50e8ca4928eafbe5f80635d
harrymkt <harrymkt@users.noreply.github.com>
2024-10-29 05:19:09 +0000

    deploy: 85250106b00c53141207294e11408e7b0b2ae1ef

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,33 +1,24 @@
 //footer.
-function addfooter()
-{
-// Create a footer element
-var footerElement = document.createElement("footer");
-
-// Create a paragraph element
-var copyrightParagraph = document.createElement("p");
-
-// Create a copyright text node
-var copyrightText = document.createTextNode("Copyright \u00A9 2024–" + new Date().getFullYear() + ', ');
-
-// Create an anchor element
-var anchorElement = document.createElement("a");
-anchorElement.href = "https://harrymkt.github.io";
-anchorElement.textContent = "Harry Min Khant.";
-
-// Append the copyright text and anchor element to the paragraph
-copyrightParagraph.appendChild(copyrightText);
-copyrightParagraph.appendChild(anchorElement);
-
-// Create another text node for "All rights reserved."
-var rightsReservedText = document.createTextNode(" All rights reserved.");
-
-// Append the rights reserved text to the paragraph
-copyrightParagraph.appendChild(rightsReservedText);
-
-// Append the paragraph to the footer
-footerElement.appendChild(copyrightParagraph);
-
-// Append the footer to the body or any other target element
-document.body.appendChild(footerElement);
+function addfooter() {
+	// Create a footer element
+	var footerElement = document.createElement("footer");
+	// Create a paragraph element
+	var copyrightParagraph = document.createElement("p");
+	// Create a copyright text node
+	var copyrightText = document.createTextNode("Copyright \u00A9 2024–" + new Date().getFullYear() + ', ');
+	// Create an anchor element
+	var anchorElement = document.createElement("a");
+	anchorElement.href = "https://harrymkt.github.io";
+	anchorElement.textContent = "Harry Min Khant.";
+	// Append the copyright text and anchor element to the paragraph
+	copyrightParagraph.appendChild(copyrightText);
+	copyrightParagraph.appendChild(anchorElement);
+	// Create another text node for "All rights reserved."
+	var rightsReservedText = document.createTextNode(" All rights reserved.");
+	// Append the rights reserved text to the paragraph
+	copyrightParagraph.appendChild(rightsReservedText);
+	// Append the paragraph to the footer
+	footerElement.appendChild(copyrightParagraph);
+	// Append the footer to the body or any other target element
+	document.body.appendChild(footerElement);
 }//End
