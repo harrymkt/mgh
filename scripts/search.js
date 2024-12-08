ff84f416e29843076a8859f4d3e5f740bebc7c2b
harrymkt <harrymkt@users.noreply.github.com>
2024-12-08 13:43:19 +0000

    deploy: d3f053b29365f7b0b630b754ac50a19259ac76a7

--- scripts/search.js
+++ scripts/search.js
@@ -15,14 +15,14 @@ function loadSearch() {
 			}
 		}
 	};
-	xhr.open('GET', "index.json");
+	xhr.open("GET", "index.json");
 	xhr.send();
 }
 loadSearch(); // call loadsearch to load the json file
 function showSearchResults() {
-	var query = searchElem.value || ''; // get the value from input
-	var searchString = query.replace(/[^\w\s]/gi, ''); // clear white spaces
-	var target = document.getElementById('searchres');
+	var query = searchElem.value || ""; // get the value from input
+	var searchString = query.replace(/[^\w\s]/gi, ""); // clear white spaces
+	var target = document.getElementById("searchres");
 	var postsByTitle = posts.reduce((acc, curr) => { // map lunr search index to your articles
 		acc[curr.title] = curr;
 		return acc;
@@ -30,32 +30,33 @@ function showSearchResults() {
 	);
 	// build lunr index file
 	var index = lunr(function () {
-		this.ref('title')
-		this.field('content')
+		this.ref("title")
+		this.field("title")
 		posts.forEach(function (doc) {
 			this.add(doc)
 		}, this)
 	});
 	// search in lunr index
-	if (searchString && searchString != '') {
+	if (searchString && searchString != "") {
 		var matches = index.search(searchString);
-		var matchPosts = [];
+		var match_posts = [];
 		matches.forEach((m) => {
-			matchPosts.push(postsByTitle[m.ref]);
+			match_posts.push(postsByTitle[m.ref]);
 		});
-		if (matchPosts.length > 0) {
+		document.getElementById("searchnum").textContent = `${match_posts.length} results`;
+		if (match_posts.length > 0) {
 			// match found with input text and lunr index
-			target.innerHTML = matchPosts.map(function (p) {
+			target.innerHTML = match_posts.map(function (p) {
 				if (p != undefined) {
-					return `<h3><a href="${p.url}"> ${p.title}</a></h3>
-						${p.content}`;
+					return `<h3><a href="${p.url}"> ${p.title}</a></h3>`;
 				}
-			}).join('');
+			}).join("");
 		} else {
 			// if no results found, then render a general message
 			target.innerHTML = `<br><h2 style="text-align:center">No search results found</h2>`;
 		};
 	} else {
-		target.innerHTML = ''
+		target.innerHTML = ""
+		document.getElementById("searchnum").textContent = ``;
 	}
 };
